6ea57aeab57ce43544c14f03799e6ad6c03b6994
Humza <humzai@hotmail.com>
2014-04-06 16:15:55 -0700

    added a test class

---
+++ Git Test 2/TRRTest2.h
@@ -0,0 +1,13 @@
+//
+//  TRRTest2.h
+//  Git Test 2
+//
+//  Created by Humza Iqbal on 4/6/14.
+//  Copyright (c) 2014 Humza Iqbal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRRTest2 : NSObject
+
+@end
---
+++ Git Test 2/TRRTest2.m
@@ -0,0 +1,13 @@
+//
+//  TRRTest2.m
+//  Git Test 2
+//
+//  Created by Humza Iqbal on 4/6/14.
+//  Copyright (c) 2014 Humza Iqbal. All rights reserved.
+//
+
+#import "TRRTest2.h"
+
+@implementation TRRTest2
+
+@end
