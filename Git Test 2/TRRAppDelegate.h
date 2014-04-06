efde17a83b301bc4f28d193184c067d638e42d7a
Humza <humzai@hotmail.com>
2014-04-06 16:03:15 -0700

    Initial Commit

---
+++ Git Test 2/TRRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TRRAppDelegate.h
+//  Git Test 2
+//
+//  Created by Humza Iqbal on 4/6/14.
+//  Copyright (c) 2014 Humza Iqbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test 2Tests/Git_Test_2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Git_Test_2Tests.m
+//  Git Test 2Tests
+//
+//  Created by Humza Iqbal on 4/6/14.
+//  Copyright (c) 2014 Humza Iqbal. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Git_Test_2Tests : XCTestCase
+
+@end
+
+@implementation Git_Test_2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
